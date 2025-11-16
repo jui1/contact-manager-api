b3eea6d79c2c58a351275e6bd31c47b7ca8b54b8
jui1 <juimandal31@gmail.com>
2025-11-16 16:28:54 +0530

    Db updated

--- config/dbConfigaration.js
+++ config/dbConfigaration.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose")
 
-const ConnectDb = async() =>{
-    try{
-
-    }catch(err){
-        console.log(err);
-        process.exit(1);
-
-    }
+const ConnectDb = async () => {
+	try {
+		const uri = process.env.MONGODB_URI || process.env.MONGO_URI
+		if (!uri) {
+			throw new Error("MONGODB_URI is not set in .env")
+		}
+		const dbName = process.env.MONGODB_DB 
+		await mongoose.connect(uri)
+		console.log("database connected")
+	} catch (err) {
+		console.log(err)
+		process.exit(1)
+	}
 };
-
 
 module.exports = ConnectDb;
--- server.js
+++ server.js
@@ -6,8 +6,17 @@ app.use(cors());
 app.use(express.json()); 
 let port = parseInt((process.env.PORT || '3001').toString().replace(/[; ]/g, '')) || 3001;
 if (port === 5000) port = 3001;
+const connectDb = require("./config/dbConfigaration");
 app.use("/api/contacts" , require("./routes/contactRoutes"));
 app.use(require("./middleware/errorHandler"));
-app.listen(port ,() =>{
-    console.log(`server runing at ${port}`);
-});
+(async () => {
+	try {
+		await connectDb();
+		app.listen(port ,() =>{
+		    console.log(`server runing at ${port}`);
+		});
+	} catch (err) {
+		console.error("Failed to start server", err);
+		process.exit(1);
+	}
+})();
